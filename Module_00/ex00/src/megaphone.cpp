efd608d256d0bf8b55a8886cfdac0cfd9b83254e
pmoreira <pmoreira@student.42lisboa.com>
2025-10-10 11:42:08 +0100

    megaphone alternative resolution

--- Module_00/ex00/src/megaphone.cpp
+++ Module_00/ex00/src/megaphone.cpp
@@ -6,16 +6,11 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 10:50:04 by pmoreira          #+#    #+#             */
-/*   Updated: 2025/10/10 11:15:28 by pmoreira         ###   ########.fr       */
+/*   Updated: 2025/10/10 11:41:30 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-unsigned char	ft_toupper(int c)
-{
-	return ((unsigned char) c - 32);
-}
 
 void	ft_shout(char *s)
 {
@@ -23,10 +18,7 @@ void	ft_shout(char *s)
 		return ;
 	while (*s)
 	{
-		if (*s >= 'a' && *s <= 'z')
-			std::cout << ft_toupper(*s);
-		else
-			std::cout << *s;
+		std::cout << (unsigned char) toupper(*s);
 		s++;
 	}
 }
@@ -42,7 +34,7 @@ int	main(int ac, char **av)
 	{
 		ft_shout(av[i]);
 		if (av[i + 1])
-		std::cout << " ";
+			std::cout << " ";
 	}
 	std::cout << std::endl;
 	return (0);
