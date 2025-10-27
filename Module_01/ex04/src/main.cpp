c0e600852a511579a953b782b140555b53cf83d0
pmoreira <pmoreira@student.42lisboa.com>
2025-10-27 16:44:43 +0000

    m1e4:working

--- Module_01/ex04/src/main.cpp
+++ Module_01/ex04/src/main.cpp
@@ -6,11 +6,29 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 16:32:25 by pmoreira          #+#    #+#             */
-/*   Updated: 2025/10/23 16:57:56 by pmoreira         ###   ########.fr       */
+/*   Updated: 2025/10/27 16:08:58 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Sed.hpp"
+
+void	replacer(std::string & buffer, std::string & target, std::string & text)
+{
+	size_t	i;
+
+	if (target.empty())
+		return;
+	i = 0;
+	while (buffer[i] && (i != std::string::npos))
+	{
+		i = buffer.find(target, i);
+		if (i == std::string::npos)
+			break;
+		buffer.erase(i, target.length());
+		if (!text.empty())
+			buffer.insert(i, text);
+	}
+}
 
 int main(int ac, char const *av[])
 {
@@ -22,11 +40,26 @@ int main(int ac, char const *av[])
 	std::ifstream infile(av[1]);
 	if (!infile)
 	{
-		std::cout << "Couldn't open the infile" << std::endl;
+		std::cerr << "Couldn't open the infile" << std::endl;
 		return (1);
 	}
-	std::string temp;
-	temp.
-	std::cout << "you shall pass" << std::endl;
+	std::string out = av[1];
+	out.append(".replace");
+	std::ofstream outfile(out.c_str());
+	if (!outfile)
+	{
+		std::cerr << "Couldn't make the outfile" << std::endl;
+		return (1);
+	}
+	std::string buffer;
+	std::string target = av[2];
+	std::string text = av[3];
+	while(std::getline(infile, buffer))
+	{
+		replacer(buffer, target, text);
+		outfile << buffer;
+		if (!infile.eof())
+			outfile << std::endl;
+	}
 	return (0);
 }
