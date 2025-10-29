b5efe793e736484c613baad3d0735938746ccc08
pmoreira <pmoreira@student.42lisboa.com>
2025-10-29 11:15:49 +0000

    m1e5: seg fault art

--- Module_01/ex05/src/main.cpp
+++ Module_01/ex05/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 11:33:26 by pmoreira          #+#    #+#             */
-/*   Updated: 2025/10/28 18:04:29 by pmoreira         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:29:33 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 int main(void)
 {
-	Harl h;
+	Harl	h;
+	int		i;
+	std::string array[5] = {"DEBUG", "INFO", "WARNING", "ERROR", "TEST"};
 
-	h.complain("DEBUG");
-	h.complain("INFO");
-	h.complain("WARNING");
-	h.complain("ERROR");
+	i = -1;
+	std::cout << std::endl << "--------------------------------" << std::endl;
+	while (++i < 5)
+		h.complain(array[i]);
+	std::cout << std::endl << "--------------------------------" << std::endl;
 	return 0;
 }
