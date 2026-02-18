bacb98b1720260c7666f1cac8fd62cf511f2f544
pmoreira <pablo147sousapt@gmail.com>
2026-02-18 14:37:30 +0000

    m8e0:main

--- Module_08/ex00/include/easyfind.hpp
+++ Module_08/ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 15:34:17 by pmoreira          #+#    #+#             */
-/*   Updated: 2026/01/29 17:00:58 by pmoreira         ###   ########.fr       */
+/*   Updated: 2026/02/18 14:12:15 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ template <typename T>
 void	containerPrinter(T container)
 {
 	int	index = 0;
-	for (typename T::iterator i = container.begin(); i < container.end(); i++)
+	for (typename T::iterator i = container.begin(); container.end(); i++)
 	{
-		std::cout << "[" << index << "] : " << *i << std::endl;
+		std::cout << "[" << index << "] :" << *i << std::endl;
 	}
 }
 
@@ -29,7 +29,6 @@ template <typename T>
 int	easyfind(T container, int to_find)
 {
 	typename T::iterator	target;
-
 	target = std::find(container.begin(), container.end(), to_find);
 	if (target != container.end())
 		return (*target);
